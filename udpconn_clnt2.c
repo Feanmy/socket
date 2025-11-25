e74c28ecdcb564e1437bad62e6a59bc9e43074fa
Feanmy <feanmy@hotmail.com>
2025-11-25 05:51:16 +0000

    测试udp连接

--- udpconn_clnt2.c
+++ udpconn_clnt2.c
@@ -1,12 +1,9 @@
 #include "common.h"
 
-/**
- * UDP使用connect()
- */
 int main(int argc, char **argv)
 {
     if (argc != 2) {
-        error(1, 0, "usage: udpconn_clnt <IPaddress>");
+        error(1, 0, "usage: udpconn_clnt2 <IPaddress>");
     }
 
     int socket_fd;
@@ -14,6 +11,7 @@ int main(int argc, char **argv)
 
     struct sockaddr_in server_addr;
     bzero(&server_addr, sizeof(server_addr));
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERV_PORT);
     inet_pton(AF_INET, argv[1], &server_addr.sin_addr);
@@ -21,49 +19,60 @@ int main(int argc, char **argv)
     socklen_t server_len = sizeof(server_addr);
 
     if (connect(socket_fd, (struct sockaddr *)&server_addr, server_len)) {
-        error(1, errno, "connect_failed");
+        error(1, errno, "connect failed");
     }
 
-    struct sockaddr *reply_addr;
-    reply_addr = malloc(server_len);
-
     char send_line[MAXLINE], recv_line[MAXLINE + 1];
-    socklen_t len;
     int n;
 
-    // 保存标准输入数据
-    while (fgets(send_line, MAXLINE, stdin) != NULL)
+    while (fgets(send_line, MAXLINE, stdin) != NULL) 
     {
         int i = strlen(send_line);
-        if (send_line[i-1] == '\n') {
-            send_line[n-1] = 0;
+        if (send_line[i - 1] == '\n') {
+            send_line[i - 1] = 0;
         }
 
         printf("now sending %s\n", send_line);
-
-        // 向服务端发送消息
-        size_t rt = sendto(socket_fd, send_line, strlen(send_line), 0, (struct sockaddr *)&server_addr, server_len);
+        size_t rt = send(socket_fd, send_line, strlen(send_line), 0);
         if (rt < 0) {
-            error(1, errno, "sendto failed");
+            error(1, errno, "send failed");
         }
-
         printf("send bytes: %zu\n", rt);
 
-        len = 0;
         recv_line[0] = 0;
-        // 接收消息
-        n = recvfrom(socket_fd, recv_line, MAXLINE, 0, reply_addr, &len);
+        n = recv(socket_fd, recv_line, MAXLINE, 0);
         if (n < 0) {
-            error(1, errno, "recvfrom failed");
+            error(1, errno, "recv failed");
         }
         recv_line[n] = 0;
-    
         fputs(recv_line, stdout);
         fputs("\n", stdout);
     }
 
     exit(0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
